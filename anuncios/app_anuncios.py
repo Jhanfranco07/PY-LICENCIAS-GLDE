d7eb8b9184d7740b81edab4f07114a91c76c6e15
Jhan Franco <jhanfrancoperez774@gmail.com>
2026-01-07 11:14:50 -0500

    FUNCION DE GENERAR CERTIFICADO

--- anuncios/app_anuncios.py
+++ anuncios/app_anuncios.py
@@ -660,7 +660,7 @@ def run_modulo_anuncios():
             "Genera un certificado y luego podrás guardarlo aquí."
         )
     else:
-        if st.button("💾 Guardar último certificado en BD Excel"):
+        if st.button("💾 Guardar certificado en Excel"):
             try:
                 guardar_certificado_en_excel(
                     ult_eval,
